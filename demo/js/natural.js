e25014fc07938ff19e9d8e09dc5a9b7043f079c9
juzibut <126956482+juzibut@users.noreply.github.com>
2023-07-21 19:03:42 +0000

    Roll up

--- demo/js/natural.js
+++ demo/js/natural.js
@@ -5121,3 +5121,273 @@ var _ = require("underscore")._;
        -
      - Declarative
      - Exclamatory
+     - Imperative
+
+  - Parts of a Sentense
+     - Subject
+     - Predicate
+
+  - Show Preposition Phrases
+*/
+
+var Sentences = function(pos, callback) {
+    this.posObj = pos;
+    this.senType = null;
+    callback(this);
+}
+
+Sentences.prototype.part = function(callback) {
+    var subject = [],
+  predicat = [],
+  verbFound = false;
+
+    this.prepositionPhrases();
+
+    for (var i = 0; i < this.posObj.tags.length; i++) {
+        if (this.posObj.tags[i].pos == "VB") {
+            if (i === 0) {
+                verbFound = true;
+            } else {
+                // We need to Test for any EX before the VB
+                if (this.posObj.tags[i - 1].pos != "EX") {
+                    verbFound = true;
+                } else {
+                    predicat.push(this.posObj.tags[i].token);
+                }
+            }
+        }
+
+        // Add Pronoun Phrase (pp) Or Subject Phrase (sp)
+        if (!verbFound) {
+            if (this.posObj.tags[i].pp != true)
+                this.posObj.tags[i].spos = "SP";
+
+            subject.push(this.posObj.tags[i].token);
+        } else {
+            if (this.posObj.tags[i].pp != true)
+                this.posObj.tags[i].spos = "PP";
+
+            predicat.push(this.posObj.tags[i].token)
+        }
+    }
+
+    if (subject.length == 0) {
+  this.posObj.tags.push({token:"You",spos:"SP",pos:"PRP",added:true});
+    }
+
+    callback(this);
+}
+
+// Takes POS and removes IN to NN or NNS
+// Adds a PP for each prepositionPhrases
+Sentences.prototype.prepositionPhrases = function() {
+    var remove = false;
+
+    for (var i = 0; i < this.posObj.tags.length; i++) {
+        if (this.posObj.tags[i].pos.match("IN")) {
+            remove = true;
+        }
+
+        if (remove) {
+            this.posObj.tags[i].pp = true;
+        }
+
+        if (this.posObj.tags[i].pos.match("NN")) {
+            remove = false;
+        }
+    }
+}
+
+Sentences.prototype.subjectToString = function() {
+    return this.posObj.tags.map(function(t){ if (t.spos == "SP" || t.spos == "S" ) return t.token }).join(' ');
+}
+
+Sentences.prototype.predicateToString = function() {
+    return this.posObj.tags.map(function(t){ if (t.spos == "PP" || t.spos == "P" ) return t.token }).join(' ');
+}
+
+Sentences.prototype.implicitYou = function() {
+    for (var i = 0; i < this.posObj.tags.length;i++) {
+        if (this.posObj.tags[i].added) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Sentences.prototype.toString = function() {
+    return this.posObj.tags.map(function(t){return t.token}).join(' ');
+}
+
+// This is quick and incomplete.
+Sentences.prototype.type = function(callback) {
+    var callback = callback || false;
+
+    // FIXME - punct seems useless
+    var lastElement = this.posObj.punct();
+    lastElement = (lastElement.length != 0) ? lastElement.pop() : this.posObj.tags.pop();
+
+    if (lastElement.pos !== ".") {
+        if (this.implicitYou()) {
+            this.senType = "COMMAND";
+        } else if (_(["WDT","WP","WP$","WRB"]).contains(this.posObj.tags[0].pos)) {
+            // Sentences that start with: who, what where when why and how, then they are questions
+            this.senType = "INTERROGATIVE";
+        } else if (_(["PRP"]).contains(lastElement.pos)) {
+            // Sentences that end in a Personal pronoun are most likely questions
+            // eg. We should run away, should we [?]
+            // eg. You want to see that again, do you [?]
+            this.senType = "INTERROGATIVE";
+        } else {
+            this.senType = "UNKNOWN";
+        }
+
+    } else {
+        switch(lastElement.token) {
+            case "?": this.senType = "INTERROGATIVE"; break;
+            case "!": this.senType = (this.implicitYou()) ? "COMMAND":"EXCLAMATORY"; break;
+            case ".": this.senType = (this.implicitYou()) ? "COMMAND":"DECLARATIVE";  break;
+        }
+    }
+
+    if (callback && _(callback).isFunction()) {
+        callback(this);
+    } else {
+        return this.senType;
+    }
+}
+
+module.exports = Sentences;
+});
+
+require.define("/lib/natural/ngrams/ngrams.js",function(require,module,exports,__dirname,__filename,process){/*
+Copyright (c) 2011, Rob Ellis, Chris Umbel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+var _ = require("underscore")._,
+    Tokenizer = require('../tokenizers/regexp_tokenizer').WordTokenizer,
+    tokenizer = new Tokenizer();
+
+exports.ngrams = function(sequence, n) {
+    return ngrams(sequence, n);
+}
+
+exports.bigrams = function(sequence) {
+    return ngrams(sequence, 2);
+}
+
+exports.trigrams = function(sequence) {
+    return ngrams(sequence, 3);
+}
+
+var ngrams = function(sequence, n) {
+    var result = [];
+
+    if (!_(sequence).isArray()) {
+        sequence = tokenizer.tokenize(sequence);
+    }
+
+    var count = _.max([0, sequence.length - n + 1]);
+
+    for (var i = 0; i < count; i++) {
+        result.push(sequence.slice(i, i + n));
+    }
+
+    return result;
+}
+
+});
+
+require.define("/lib/natural/distance/jaro-winkler_distance.js",function(require,module,exports,__dirname,__filename,process){/*
+Copyright (c) 2012, Adam Phillabaum, Chris Umbel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+Unless otherwise stated by a specific section of code
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+// Computes the Jaro distance between two string -- intrepreted from:
+// http://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
+// s1 is the first string to compare
+// s2 is the second string to compare
+function distance(s1, s2) {
+    if (typeof(s1) != "string" || typeof(s2) != "string") return 0;
+    if (s1.length == 0 || s2.length == 0)
+        return 0;
+    s1 = s1.toLowerCase(), s2 = s2.toLowerCase();
+    var matchWindow = (Math.floor(Math.max(s1.length, s2.length) / 2.0)) - 1;
+    var matches1 = new Array(s1.length);
+    var matches2 = new Array(s2.length);
+    var m = 0; // number of matches
+    var t = 0; // number of transpositions
+
+    //debug helpers
+    //console.log("s1: " + s1 + "; s2: " + s2);
+    //console.log(" - matchWindow: " + matchWindow);
+
+    // find matches
+    for (var i = 0; i < s1.length; i++) {
+  var matched = false;
+
+  // check for an exact match
+  if (s1[i] ==  s2[i]) {
+    matches1[i] = matches2[i] = matched = true;
+    m++
+  }
+
+  // check the "match window"
+  else {
+          // this for loop is a little brutal
+          for (k = (i <= matchWindow) ? 0 : i - matchWindow;
+            (k <= i + matchWindow) && k < s2.length && !matched;
+      k++) {
+                if (s1[i] == s2[k]) {
+                    if(!matches1[i] && !matches2[k]) {
+                          m++;
+                  }
+
+                  matches1[i] = matches2[k] = matched = true;
+              }
+          }
+  }
+    }
+
+    if(m == 0)
+        return 0.0;
+
+    // count transpositions
